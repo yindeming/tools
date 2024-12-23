edaad0cc698552d6875bb0e05ba701c0be4a2d41
iniwex5 <35565716+iniwex5@users.noreply.github.com>
2024-12-23 20:26:00 +0800

    Update NC_QB438.sh

--- NC_QB438.sh
+++ NC_QB438.sh
@@ -10,7 +10,7 @@ PASSWORD=$2
 PORT=${3:-8080}
 UP_PORT=${4:-23333}
 RAM=$(free -m | awk '/^Mem:/{print $2}')
-CACHE_SIZE=$((RAM / 4))
+CACHE_SIZE=$((RAM / 8))
 
 bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) -u $USER -p $PASSWORD -c $CACHE_SIZE -q 4.3.9 -l v1.2.19 -x
 apt install -y curl htop vnstat
